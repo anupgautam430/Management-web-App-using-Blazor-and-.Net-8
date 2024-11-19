6e743ef41e63d2ac7cff18ac084372daaf9a469d
Anup Gautam <anupgautam430@gmail.com>
2024-11-19 22:23:18 +0545

    employee-crud-completed

--- BlazorCrudProject/DataAccess/Services/EmployeeService.cs
+++ BlazorCrudProject/DataAccess/Services/EmployeeService.cs
@@ -53,5 +53,62 @@ namespace BlazorCrudProject.DataAccess.Services
             }
         }
 
+        public EmployeeViewModel? FindEmployee(int employeeId)
+        {
+            var employee = dbContext.Employees.Find(employeeId);
+            if (employee == null) return null;
+
+            EmployeeViewModel result = new EmployeeViewModel
+            {
+                EmployeeId = employee.EmployeeId,
+                FullName = employee.FullName,
+                Department = employee.Department,
+                DateOfBirth = employee.DateOfBirth,
+                Age = employee.Age,
+                PhoneNumber = employee.PhoneNumber,
+            };
+
+            return result;
+        }
+
+        public bool UpdateEmployee(EmployeeViewModel model)
+        {
+            try
+            {
+                var employee = dbContext.Employees.Find(model.EmployeeId);
+                if (employee == null) return false;
+
+                employee.FullName = model.FullName;
+                employee.Department = model.Department;
+                employee.DateOfBirth = model.DateOfBirth;
+                employee.Age = model.Age;
+                employee.PhoneNumber = model.PhoneNumber;
+
+                var result = dbContext.SaveChanges();
+                return (result > 0);
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+           
+        }
+
+        public bool DeleteEmployee(int employeeId)
+        {
+            try
+            {
+                var employee = dbContext.Employees.Find(employeeId);
+                if (employee == null) return false;
+
+                dbContext.Employees.Remove(employee);
+                var result = dbContext.SaveChanges();
+                return (result > 0);
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
     }
 }
